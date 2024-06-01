f95d23b7791203456c02cdabf82a9d490fce4c1a
aftahi334 <aftahi334@gmail.com>
2024-05-31 19:13:18 -0500

    inspect a request's query string

--- main.go
+++ main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+type keyServ string
+
+const keyServAddr = keyServ("serverAddr")
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+		ctx.Value(keyServAddr),
+		hasFirst, first,
+		hasSecond, second)
+
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServAddr))
+	io.WriteString(w, "Hello HTTP!\n")
+}
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	ctx := context.Background()
+	server := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error listening for server: %s\n", err)
+	}
+
+}
